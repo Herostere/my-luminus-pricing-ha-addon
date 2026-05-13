4899925cd77eb470f625b320750837bc5bca19ba
Herostere <github.com.travesty689@passmail.net>
2026-05-13 11:45:27 +0200

    Update data update logic to avoid getting unavailable

--- custom_components/my_luminus_pricing/coordinator.py
+++ custom_components/my_luminus_pricing/coordinator.py
@@ -17,7 +17,7 @@ from .const import DEFAULT_SCAN_INTERVAL, GAS_M3_TO_KWH
 import logging
 
 _LOGGER = logging.getLogger(__name__)
-
+UPDATE_TRIES = 0
 
 class LuminusCoordinator(DataUpdateCoordinator):
     """My example coordinator."""
@@ -90,105 +90,113 @@ class LuminusCoordinator(DataUpdateCoordinator):
         This is the place to retrieve and pre-process the data into an appropriate data structure
         to be used to provide values for all your entities.
         """
-        try:
-            # ----------------------------------------------------------------------------
-            # Get the data from your api
-            # NOTE: Change this to use a real api call for data
-            # ----------------------------------------------------------------------------
-            
-            await self.hass.async_add_executor_job(self.api.login)
-            meters = await self.hass.async_add_executor_job(self.api.get_meters)
-            data = []
+        while True:
+            try:
+                # ----------------------------------------------------------------------------
+                # Get the data from your api
+                # NOTE: Change this to use a real api call for data
+                # ----------------------------------------------------------------------------
+                
+                await self.hass.async_add_executor_job(self.api.login)
+                meters = await self.hass.async_add_executor_job(self.api.get_meters)
+                data = []
 
-            current_month = ((datetime.now().month - 5) % 12) + 1
+                current_month = ((datetime.now().month - 5) % 12) + 1
 
-            for meter in meters['meters']:
-                eanNr = meter['ean']
-                energyType = meter['energyType']
-                meterDetails = await self.hass.async_add_executor_job(self.api.get_meter, eanNr)
-                consumptionDetails = await self.hass.async_add_executor_job(self.api.get_current_consumption, eanNr)
-                budgetDetails = await self.hass.async_add_executor_job(self.api.get_advance_and_paid)
+                for meter in meters['meters']:
+                    eanNr = meter['ean']
+                    energyType = meter['energyType']
+                    meterDetails = await self.hass.async_add_executor_job(self.api.get_meter, eanNr)
+                    consumptionDetails = await self.hass.async_add_executor_job(self.api.get_current_consumption, eanNr)
+                    budgetDetails = await self.hass.async_add_executor_job(self.api.get_advance_and_paid)
 
-                if meterDetails and consumptionDetails and budgetDetails:
-                    pname = meterDetails['productName']
-                    prices = meterDetails['prices']
-                    meterType = "dual" if "dual" in prices else meterDetails['activeMeterType']
-                    meterPrices = prices[meterType]
-                    budget_billing = budgetDetails[0] if budgetDetails[0].get("ean") == eanNr else budgetDetails[1]
+                    if meterDetails and consumptionDetails and budgetDetails:
+                        pname = meterDetails['productName']
+                        prices = meterDetails['prices']
+                        meterType = "dual" if "dual" in prices else meterDetails['activeMeterType']
+                        meterPrices = prices[meterType]
+                        budget_billing = budgetDetails[0] if budgetDetails[0].get("ean") == eanNr else budgetDetails[1]
 
-                    # This can cause issues when the final bill is due. Adding "or" to fix that.
-                    remaining_months = (budget_billing.get("simulation") or {}).get("openAdvancesCount") or 1
-                    already_paid = (budget_billing.get("simulation")or {}).get("totalPaidAmount") or 0
-                    # TODO save already_paid in a file. If value = 0, maybe it is best to take the value from the file. 
-                    # The issue is that we can't get the "already paid" value when My Luminus is not allowing to adjust what we pay.
+                        # This can cause issues when the final bill is due. Adding "or" to fix that.
+                        remaining_months = (budget_billing.get("simulation") or {}).get("openAdvancesCount") or 1
+                        already_paid = (budget_billing.get("simulation")or {}).get("totalPaidAmount") or 0
+                        # TODO save already_paid in a file. If value = 0, maybe it is best to take the value from the file. 
+                        # The issue is that we can't get the "already paid" value when My Luminus is not allowing to adjust what we pay.
 
-                    period_quantities = consumptionDetails.get("periodQuantities", {})
+                        period_quantities = consumptionDetails.get("periodQuantities", {})
 
-                    device = {
-                        'device_id': eanNr,
-                        'device_name': pname + ' (' + eanNr + ')',
-                        'device_type': energyType,
-                        'product_name': pname
-                    }
-                    data.append(device)
+                        device = {
+                            'device_id': eanNr,
+                            'device_name': pname + ' (' + eanNr + ')',
+                            'device_type': energyType,
+                            'product_name': pname
+                        }
+                        data.append(device)
 
-                    for propName, price in meterPrices.items():
-                        device[propName] = price['rate'] / (1 if propName == 'fixed' else 100)
+                        for propName, price in meterPrices.items():
+                            device[propName] = price['rate'] / (1 if propName == 'fixed' else 100)
 
-                    if energyType == "Gas":
-                        gas_m3 = period_quantities.get("offtake", 0)
-                        gas_kwh = gas_m3 * GAS_M3_TO_KWH
-                        gas_price = device.get("single", 0)
+                        if energyType == "Gas":
+                            gas_m3 = period_quantities.get("offtake", 0)
+                            gas_kwh = gas_m3 * GAS_M3_TO_KWH
+                            gas_price = device.get("single", 0)
 
-                        cost_so_far = gas_kwh * gas_price
-                        forecast_remaining = self._forecast_remaining_cost(cost_so_far, current_month, remaining_months, "gas")
-                        projected_total = cost_so_far + forecast_remaining
+                            cost_so_far = gas_kwh * gas_price
+                            forecast_remaining = self._forecast_remaining_cost(cost_so_far, current_month, remaining_months, "gas")
+                            projected_total = cost_so_far + forecast_remaining
 
-                        device["estimated_cost"] = (projected_total - already_paid) / remaining_months 
-                        # device["estimated_cost"] = ((gas_kwh * gas_price) - already_paid) / remaining_months
-                        
-                        _LOGGER.warning("Gas debug ean=%s gas_m3=%s gas_kwh=%s gas_price=%s already_paid=%s remaining_months=%s current_month=%s", eanNr, gas_m3, gas_kwh, gas_price, already_paid, remaining_months, current_month,)
-                        _LOGGER.warning("Forecast debug cost_so_far=%s forecast_remaining=%s projected_total=%s estimated=%s", cost_so_far, forecast_remaining, projected_total, (projected_total - already_paid) / remaining_months if remaining_months else None,)
-
-                    elif energyType == "Electricity":
-                        day_kwh = 0
-                        night_kwh = 0
-
-                        for detail in period_quantities.get("details", []):
-                            if detail.get("direction") != "Offtake":
-                                continue
-
-                            if detail.get("timeFrame") == "Day":
-                                day_kwh = detail.get("quantity", 0)
-                            elif detail.get("timeFrame") == "Night":
-                                night_kwh = detail.get("quantity", 0)
-
-                        device["electricity_consumption_day_kwh"] = day_kwh
-                        device["electricity_consumption_night_kwh"] = night_kwh
-
-                        day_price = 0
-                        night_price = 0
-                        if meterType == "dual":
-                            day_price = device.get("dualDay", 0)
-                            night_price = device.get("dualNight", 0)
+                            device["estimated_cost"] = (projected_total - already_paid) / remaining_months 
+                            # device["estimated_cost"] = ((gas_kwh * gas_price) - already_paid) / remaining_months
                             
-                        cost_so_far = (day_kwh * day_price) + (night_kwh * night_price)
-                        forecast_remaining = self._forecast_remaining_cost(cost_so_far, current_month, remaining_months, "electricity")
-                        projected_total = cost_so_far + forecast_remaining
+                            _LOGGER.warning("Gas debug ean=%s gas_m3=%s gas_kwh=%s gas_price=%s already_paid=%s remaining_months=%s current_month=%s", eanNr, gas_m3, gas_kwh, gas_price, already_paid, remaining_months, current_month,)
+                            _LOGGER.warning("Forecast debug cost_so_far=%s forecast_remaining=%s projected_total=%s estimated=%s", cost_so_far, forecast_remaining, projected_total, (projected_total - already_paid) / remaining_months if remaining_months else None,)
 
-                        device["estimated_cost"] = (projected_total - already_paid) / remaining_months    
-                        # device["estimated_cost"] = ((day_kwh * day_price) + (night_kwh * night_price) - already_paid) / remaining_months
-                    
-            #await self.hass.async_add_executor_job(self.api.logout)
-            _LOGGER.info('Data updated.')
-            #_LOGGER.warning('updated coordinator data', data)  
+                        elif energyType == "Electricity":
+                            day_kwh = 0
+                            night_kwh = 0
 
-        except Exception as err:
-            # This will show entities as unavailable by raising UpdateFailed exception
-            raise UpdateFailed(f"Error communicating with API: {err}") from err
+                            for detail in period_quantities.get("details", []):
+                                if detail.get("direction") != "Offtake":
+                                    continue
 
-        # What is returned here is stored in self.data by the DataUpdateCoordinator
-        return data
+                                if detail.get("timeFrame") == "Day":
+                                    day_kwh = detail.get("quantity", 0)
+                                elif detail.get("timeFrame") == "Night":
+                                    night_kwh = detail.get("quantity", 0)
+
+                            device["electricity_consumption_day_kwh"] = day_kwh
+                            device["electricity_consumption_night_kwh"] = night_kwh
+
+                            day_price = 0
+                            night_price = 0
+                            if meterType == "dual":
+                                day_price = device.get("dualDay", 0)
+                                night_price = device.get("dualNight", 0)
+                                
+                            cost_so_far = (day_kwh * day_price) + (night_kwh * night_price)
+                            forecast_remaining = self._forecast_remaining_cost(cost_so_far, current_month, remaining_months, "electricity")
+                            projected_total = cost_so_far + forecast_remaining
+
+                            device["estimated_cost"] = (projected_total - already_paid) / remaining_months    
+                            # device["estimated_cost"] = ((day_kwh * day_price) + (night_kwh * night_price) - already_paid) / remaining_months
+                        
+                #await self.hass.async_add_executor_job(self.api.logout)
+                _LOGGER.info('Data updated.')
+                UPDATE_TRIES = 0
+                #_LOGGER.warning('updated coordinator data', data)  
+
+            except Exception as err:
+                _LOGGER.warning(f"Error communicating with API in async_update_data: {err}")
+                UPDATE_TRIES += 1
+                time.sleep(15 * UPDATE_TRIES)
+                continue
+
+            # except Exception as err:
+                # This will show entities as unavailable by raising UpdateFailed exception
+                #raise UpdateFailed(f"Error communicating with API: {err}") from err
+
+            # What is returned here is stored in self.data by the DataUpdateCoordinator
+            return data
 
     # ----------------------------------------------------------------------------
     # Here we add some custom functions on our data coordinator to be called
